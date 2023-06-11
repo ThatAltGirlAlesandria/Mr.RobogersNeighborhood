aa6b09eed64cd8d02373381c276d819948a439ad
ThatAltGirlAlesandria <134242341+ThatAltGirlAlesandria@users.noreply.github.com>
2023-06-10 21:15:55 -0700

    WIP adding HTML and css styles to check if js is functional.

---
+++ js/scripts.js
@@ -0,0 +1,36 @@
+function beepBoop(input) {
+  let inputValue = []
+  const inputNumber = parseInt(input);
+    for(let i = 0; i <= inputNumber; i+=1) {
+        inputValue.push(i.toString());
+    }
+    return inputValue;
+  }
+
+
+function ifMrRobogersNeighborhood(inputVal){
+  const roboArray = inputVal.map((elemnet) => {
+    if (element.includes("3")) {
+      return "Won't you be my neighbor?";
+    } else if (element.includes("2")) {
+      return "Boop";
+    } else if (element.includes("1")){
+      return "Beep";
+    } else {
+      return element;
+    }});
+    return roboArray.join(" ");
+};
+
+function mrRobogersNeighborhood(e){
+  const userInput = document.getElementById("input-number").value;
+  const newValue = ifMrRobogersNeighborhood(beepBoop(userInput));
+  const userOutput = document.getElementById("outPut");
+  userOutput.innerText = newValue;
+  userOutput.removeAttribute("class");
+}
+
+window.addEventListener("load", function() {
+  const form = document.getElementById("user-input");
+  form.addEventListener("submit", mrRobogersNeighborhood)
+});
